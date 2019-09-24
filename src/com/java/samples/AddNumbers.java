6171353c87b320fa128b3ad3e9d595ee3a2ea77f
chandanakrishna <chandanakrishna92@gmail.com>
2019-09-24 23:19:12 +0530

    Added a new class

---
+++ src/com/java/samples/AddNumbers.java
@@ -0,0 +1,12 @@
+package com.java.samples;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sum is 100");
+		
+	}
+
+}
