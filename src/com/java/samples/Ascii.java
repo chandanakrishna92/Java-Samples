7477a42c7e0fed46ce4cb00875bd4ee94f2a945d
chandanakrishna <chandanakrishna92@gmail.com>
2019-10-16 20:57:16 +0530

    Ascii values

--- src/com/java/samples/Ascii.java
+++ src/com/java/samples/Ascii.java
@@ -2,8 +2,8 @@ package com.java.samples;
 
 public class Ascii {
 	public static void main(String[] args) {
-		char ch1='A';
-		int b=(int)ch1;
+		char ch1 = 'A';
+		int b = (int) ch1;
 		System.out.println(b);
 	}
 
