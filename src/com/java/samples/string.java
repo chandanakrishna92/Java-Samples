ee507172d76136b9f8576657bd95f681181a9915
chandanakrishna <chandanakrishna92@gmail.com>
2019-10-16 20:51:21 +0530

    committed Reverse String program

---
+++ src/com/java/samples/string.java
@@ -0,0 +1,15 @@
+package com.java.samples;
+
+public class string {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		char[] ch = s1.toCharArray();
+		for (int i = 3; i >= 0; i--) {
+			System.out.println(ch[i]);
+
+		}
+
+	}
+
+}
